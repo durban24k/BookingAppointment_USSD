781727501d432de8abe3150474a8eb7cb119c24d
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-03-25 16:49:54 +0300

    v2.0.0

--- app.js
+++ app.js
@@ -32,15 +32,58 @@ app.post('*',(req,res)=>{
      let count=text.split('*').length;
      let txt=text.split('*');
 
+     // Date function
+     function addDays(days){
+          let date = new Date();
+          return date.toLocaleDateString('en-GB',date.setDate(date.getDate()+days))
+     }
+
      if(text==''){
           response=`CON Welcome to OPTICAL CARE
           1. Book an Appointment
           2. Exit`;
      }else if(txt[0]==='1' && count===1){
           response=`CON Enter your First Name`;
+     }else if(count===2){
+          response=`CON Enter your Last Name`;
+     }else if(count===3){
+          response=`CON Select Appointment Date:
+          1. ${addDays(0)} (Today)
+          2. ${addDays(1)}
+          3. ${addDays(2)}
+          4. ${addDays(3)}
+          5. ${addDays(4)}`;
+     }else if(count===4){
+          response=`CON Select Appointment Time:
+          1. 9:00 AM
+          2. 10:00 AM
+          3. 11:00 AM
+          4. 12:00 NOON
+          5. 2:00 PM
+          6. 3:00 PM`;
+     }else if(count===5){
+          response=`CON Select Appointment Type:
+          1. Eye Test
+          2. Frame Replacement
+          3. Doctor Consultation`;
+     }else if(count===6){
+          response=`CON Confirm Appointment:
+          Name:
+          Phone No:
+          Date:
+          Time:
+          Type:
+          
+          1. Book Appointment
+          2. Cancel`;
+     }else if(count===7){
+          response=`END Appointment Booked Successfully!
+          Thank you for using our service.`;
+     }else if(count===7){
+          response=`End Booking Cancelled!
+          Call +254722000000 for any assistance.`;
      }else if(txt[0]==='2'){
-          response=`END You did not book an appointment.
-          Thank you for using our service`;
+          response=`END Session Cancelled`;
      }else{
           res.status(400);
           response=`END TRY AGAIN
